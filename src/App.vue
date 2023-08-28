c2908e2e487c19077d6e13de07107285fa79c6e6
Ryan <656040874@qq.com>
2023-08-28 10:34:39 +0800

    feat(components): 基本完成

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Scene from '@/components/Scene.vue'
+</script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Scene></Scene>
 </template>
 
-<style scoped>
-.logo {
-  position: relative;
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<!-- <style scoped lang="scss"></style> -->
